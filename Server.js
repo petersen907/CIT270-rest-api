d0d3aae3f96f2de6ce9bce28f08adb58c49639a3
petersen907 <55543355+petersen907@users.noreply.github.com>
2022-01-28 13:54:25 -0700

    Add md5 to encrypt password

--- Server.js
+++ Server.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const bodyParser = require('body-parser');
 
+var md5 = require('md5');
+
 const port = 3000;
 
 const app = express();
@@ -13,7 +15,7 @@ res.send("Hello browser, updated!");
 
 app.post('/login', (req,res) =>{
     console.log(JSON.stringify(req.body));
-    if(req.body.userName == "justmejuice" && req.body.password == "cangetin"){
+    if(req.body.userName == "justmejuice" && md5(req.body.password) == 'a4a254cc265d75222df6d97cfb759297'){  
         res.send("Welcome!")
      }else{
         res.send("Who goes there?")
