e040e41c3c64f61428b5f65d1e0a4ded55acbaa4
petersen907 <55543355+petersen907@users.noreply.github.com>
2022-01-28 13:13:53 -0700

    Added loginscript.bat

--- Server.js
+++ Server.js
@@ -1,11 +1,23 @@
 const express = require('express')
+const bodyParser = require('body-parser');
 
 const port = 3000;
 
 const app = express();
 
+app.use(bodyParser.json());
+
 app.get('/', (req, res) => {
-res.send("Hello browse");
+res.send("Hello browser, updated!");
+});
+
+app.post('/login', (req,res) =>{
+    console.log(JSON.stringify(req.body));
+    if(req.body.userName == "justmejuice" && req.body.password == "cangetin"){
+        res.send("Welcome!")
+     }else{
+        res.send("Who goes there?")
+        }
 });
 
 app.listen(port, () =>{});
