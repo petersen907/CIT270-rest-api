7692028f2f2cbef698fbc1125a22b4ae3923ff5c
petersen907 <55543355+petersen907@users.noreply.github.com>
2022-02-02 13:49:35 -0700

    Add OpenSSL

--- Server.js
+++ Server.js
@@ -1,17 +1,22 @@
 const express = require('express')
 const bodyParser = require('body-parser');
-
+const https = require('https')
+const fs = require('fs')
+const port = 3000;
+const app = express();
 var md5 = require('md5');
 
-const port = 3000;
-
-const app = express();
-
 app.use(bodyParser.json());
-
 app.get('/', (req, res) => {
-res.send("Hello browser, updated!");
+res.send("Hello HTTPS!");
 });
+
+https.createServer({
+    key: fs.readFileSync('server.key'),
+    cert: fs.readFileSync('server.cert')
+  }, app).listen(3000, () => {
+    console.log('Listening...')
+  })
 
 app.post('/login', (req,res) =>{
     console.log(JSON.stringify(req.body));
@@ -22,5 +27,4 @@ app.post('/login', (req,res) =>{
         }
 });
 
-app.listen(port, () =>{});
-
+//app.listen(port, () =>{});
